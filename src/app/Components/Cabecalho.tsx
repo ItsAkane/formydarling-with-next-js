67d100eb098052257bcaaea69718cb285fd3d228
ItsAkaNe <gabriel.kosvoski@outlook.com>
2025-06-03 15:26:19 -0300

    included new page and jpgs

--- src/app/Components/Cabecalho.tsx
+++ src/app/Components/Cabecalho.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export function Cabecalho() {
     return (
         <>
             <div className="fixed top-0 left-0 px-[50px] pt-[10px] pb-[20px] w-full flex justify-end gap-[75px] bg-[#ff80b3] box-border mb-[20px] mr-[50px]">
 
                 <div className="fixed left-[30px] top-6 px-10 text-white">
-                    <h3 id="For_My_Darling">For My Darling</h3>
+                    <Link href="/"><h3 id="For_My_Darling">For My Darling &lt;3</h3></Link>
                 </div>
 
-                <a href="html/teamo.html" className="inline-block gb-[#ff4d94] text-white no-underline px-5 py-2.5 rounded-[20px] font-bold shadow-md text-[18px] transition hover:[#e60073]">Carta </a>
-                <a href="html/lembrancas.html" className="inline-block gb-[#ff4d94] text-white no-underline px-5 py-2.5 rounded-[20px] font-bold shadow-md text-[18px] transition hover:[#e60073]">Lembrancas </a>
+                <Link href="/teamo"><div className="inline-block gb-[#ff4d94] text-white no-underline px-5 py-2.5 rounded-[20px] font-bold shadow-md text-[18px] transition hover:[#e60073]">Carta &lt;3</div></Link>
+                <Link href=""><div className="inline-block gb-[#ff4d94] text-white no-underline px-5 py-2.5 rounded-[20px] font-bold shadow-md text-[18px] transition hover:[#e60073]">Lembrancas &lt;3</div></Link>
             </div >
         </>
     )
---
+++ src/app/teamo/page.tsx
@@ -0,0 +1,19 @@
+import { Cabecalho } from "../Components/Cabecalho";
+
+export default function TeAmo() {
+    return (
+        <>
+            <Cabecalho />
+
+            <img src="/img/img_viada.jpeg" alt="emoji com coracoes" className="w-[200px] h-auto rounded-[15px] shadow-md border-[3px] border-[#ff80b3] mb-5"/>
+            <h3>
+                Você é a pessoa mais especial com quem já tive contato na minha vida.<br />
+                Obrigado por sempre estar comigo, mesmo quando faço merda ou te deixo triste.<br />
+                Sempre vou estar tentando mudar e melhorar, pra você ter a minha melhor versão.<br />
+                <br />
+                Eu te amo &lt;3,<br />
+                do seu nerdolinha sz.
+            </h3>
+        </>
+    )
+}
