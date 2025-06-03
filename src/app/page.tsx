0dde1eaa45da5ebec73cb0ee214cf891abb8b8a2
ItsAkaNe <gabriel.kosvoski@outlook.com>
2025-06-03 15:36:02 -0300

    song width alt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Cabecalho />
-      <iframe style={{ borderRadius: "12px", marginTop: "80px",  }} src="https://open.spotify.com/embed/track/1DLKuppSYytOuxhtI6KBGu?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe style={{ borderRadius: "12px", marginTop: "80px" }} src="https://open.spotify.com/embed/track/1DLKuppSYytOuxhtI6KBGu?utm_source=generator&theme=0" width="500px" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       <Temporizer/>
     </>
   )
